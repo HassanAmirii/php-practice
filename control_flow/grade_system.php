ecdde626dc9b483b8075a81289daf48bb01eff22
Hassan2bit <hassanamiri.ai@gmail.com>
2026-03-18 19:02:36 +0100

    finish grade_system

--- control_flow/grade_system.php
+++ control_flow/grade_system.php
@@ -1,3 +1,5 @@
+<?php
+/*
 Exercise 1: Multi-Tier Grading System
 Initialize a variable score with an integer.
 
@@ -14,3 +16,39 @@ Below 70: "F"
 Use a switch statement on the grade variable to echo a custom message (e.g., "A" echoes "Excellent").
 
 Include a default case for invalid grades.
+*/
+
+$score = 40;
+$grade = null;
+
+if ($score >= 90){
+    $grade = "A";
+}elseif($score >= 80){
+    $grade = "B";
+}elseif($score >= 70) {
+    $grade = "C";
+}else{
+    $grade = "F";
+}
+
+#Use a switch statement on the grade variable to echo a custom message (e.g., "A" echoes "Excellent").
+
+switch($grade){
+    case ($grade === "A"):
+        echo "Excellent";
+        break;
+    
+    case ($grade === "B"):
+        echo "Great";
+        break;
+
+    case ($grade === "C"):
+        echo "Nice";
+        break;
+        
+    default:
+        echo "You failed";    
+
+}
+
+?>
