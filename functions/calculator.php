dc66f1cc5a17c61005e34f05844afb4b166b13c3
Hassan2bit <hassanamiri.ai@gmail.com>
2026-03-19 11:29:33 +0100

    created new files to work on exercises

---
+++ functions/calculator.php
@@ -0,0 +1,20 @@
+<?php 
+/*
+
+Exercise 1: The Calculator with Type Safety
+Enable Strict Typing at the top of the script.
+
+Define a function that accepts two float parameters and returns a float.
+
+The function should multiply the two numbers.
+
+Call the function with an integer and a float.
+
+Call the function again, but pass a string number (e.g., "5.5") and observe the terminal error.
+
+Expected Outcome: Success on the first call due to float/int compatibility in strict mode; Fatal Error on the second call.
+*/
+
+
+
+?>
---
+++ functions/scope_tracking.php
@@ -0,0 +1,19 @@
+<?php 
+
+/*
+Exercise 2: Global vs. Static Scope Tracking
+Define a global variable count initialized to 0.
+
+Create a function that uses a static variable to increment and echo a value every time the function is called.
+
+Inside the same function, try to echo the global count variable.
+
+Call the function three times.
+
+Expected Outcome: The static variable increments (1, 2, 3) 
+while the global variable remains inaccessible unless the global keyword is used.
+
+*/
+
+
+?>
