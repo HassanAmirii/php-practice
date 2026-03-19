b0a70411d922ac0a3276351b96110cce6e174148
Hassan2bit <hassanamiri.ai@gmail.com>
2026-03-19 11:50:32 +0100

    finished calculator in functions

--- functions/calculator.php
+++ functions/calculator.php
@@ -15,6 +15,18 @@ Call the function again, but pass a string number (e.g., "5.5") and observe the 
 Expected Outcome: Success on the first call due to float/int compatibility in strict mode; Fatal Error on the second call.
 */
 
+#enable strict typing 
+declare(strict_types=1);
 
+function multiplyFloat(float $a, float $b) :float {
+    return $a * $b;
+}
+
+#call with an int and a float
+echo multiplyFloat(4, 4.4);
+
+#call with a string and a float
+
+// echo multiplyFloat("40", 4.4);
 
 ?>
